7e62bd3199d5a022da31dd09fccecf663b2d4d20
Renaud-HUSSON <renaudhusson09@gmail.com>
2020-11-29 22:12:50 +0100

    404 Not found page & route

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -13,31 +13,33 @@ import Login from "../pages/Login"
 import PrivateRoute from './shared/Routes/PrivateRoute'
 //Context
 import { RedirectContextProvider } from "./shared/Context/RedirectContext"
+import NotFound from "./shared/NotFound"
 
 const RouteSwitch = () => {
   const location = useLocation()
   const categories = useFetchData('http://localhost/astroshoot/api/categories/read.php')
 
-  return <Switch location={location} key={location.key}>
-  <Route path="/" exact component={Accueil} />
-  {
-    !categories[1] 
-    ? categories[0]['data'].map((categorie) => {
-      return <Route key={categorie.id} path={`/${categorie.nom}`} exact>
-          <Categorie categorie={categorie.nom} title={categorie.titre}/>
-        </Route>
-      })
-      : ''
-    }
-  <Route path="/:categorie/:id" exact component={ImageDetails} />
-  <Route path="/materiel" exact component={Materiel} />
-  <Route path="/informations" exact component={Informations}/>
-  <Route path="/calculs" exact component={Calculs}/>
-  <RedirectContextProvider>
-    <Route path="/login" exact component={Login}/>
-    <PrivateRoute path="/admin" component={Admin}/>
+  return <RedirectContextProvider>
+    <Switch location={location} key={location.key}>
+      <Route path="/" exact component={Accueil} />
+      <Route path="/login" exact component={Login}/>
+      <PrivateRoute path="/admin" component={Admin}/>
+      {
+        !categories[1] 
+        ? categories[0]['data'].map((categorie) => {
+          return <Route key={categorie.id} path={`/${categorie.nom}`} exact>
+              <Categorie categorie={categorie.nom} title={categorie.titre}/>
+            </Route>
+          })
+          : ''
+        }
+      <Route path="/:categorie/:id" exact component={ImageDetails} />
+      <Route path="/materiel" exact component={Materiel} />
+      <Route path="/informations" exact component={Informations}/>
+      <Route path="/calculs" exact component={Calculs}/>
+      <Route component={NotFound}/>
+    </Switch>
   </RedirectContextProvider>
-</Switch>
 }
 
 export default RouteSwitch
---
+++ src/components/shared/NotFound.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return <div className="not-found-container">
+    <div className="not-found">
+      <h1>404</h1>
+      <p>PAGE NOT FOUND</p>
+    </div>
+  </div>
+}
+
+export default NotFound
